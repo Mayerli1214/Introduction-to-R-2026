b7f39e0d565460a1f3e2f514ab7286d4ce3b2cbc
Mayerli Mercado <mayerlimercado@Mayerlis-MacBook-Air.local>
2026-09-25 19:22:16 -0400

    AS2

---
+++ Clases/Plots.R
@@ -0,0 +1,26 @@
+# Plots in Base R ------------------
+
+data(DNase)
+View(DNase)
+
+
+head(DNase, 3)
+?DNase
+
+mean_density <- aggregate(density~conc, 
+                          data=DNase,
+                          FUN=mean)
+
+mean_density
+
+barplot(
+  height = mean_density$density,
+  names.arg = mean_density$conc,
+  col = "lightblue",
+  border = "grey30",
+  main = "Mean DNase I Activity by Concentration",
+  xlab = "Concentration (mg/mL)",
+  ylab = "Mean Optical Density",
+  las = 2, 
+  cex.names = 0.5
+)
